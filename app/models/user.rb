dc6739e3db625ffebabf075da906a3a5e99527e8
Freika <iamfrey@gmail.com>
2014-07-02 22:58:48 +0700

    Added: bootstrap, model relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :invitable, 
+             :database_authenticatable, 
+             :registerable,
+             :recoverable, 
+             :rememberable, 
+             :trackable#, 
+             #:validatable
+  has_many :posts
 end
