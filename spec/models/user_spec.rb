bb7e73ef6af42d71cdb9b1064f67e0a033c56048
Freika <iamfrey@gmail.com>
2014-07-03 22:03:32 +0700

    Added: test for registration w/out invitetaion

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can register without invitation" do
+    user = User.create!(username: "Test", email: "email@email.ru", password: "looooooooongpassword")
+  end
 end
