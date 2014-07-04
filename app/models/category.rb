80e970a7e6a608bd33423115baf4b327e9b88968
Freika <iamfrey@gmail.com>
2014-07-04 22:55:23 +0700

    Added: Navbar

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  url        :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Category < ActiveRecord::Base
+  validates :title, :url, presence: true, length: { minimum: 3 }
+  validates :url, uniqueness: true
   has_many :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  url        :string(255)
+#  content    :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Post < ActiveRecord::Base
+  validates :title, :url, :content, presence: true, length: { minimum: 3 }
+  validates :url, uniqueness: true
   belongs_to :category
 end
