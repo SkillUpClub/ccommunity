d670432d427d4734581a91829f4c2231516296a6
Freika <iamfrey@gmail.com>
2014-07-04 23:33:06 +0700

    Added some beauty to views

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,5 @@ class Post < ActiveRecord::Base
   validates :title, :url, :content, presence: true, length: { minimum: 3 }
   validates :url, uniqueness: true
   belongs_to :category
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users#, :controllers => { :registrations => :registrations }
    as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
     put 'users' => 'devise/registrations#update', :as => 'user_registration'
