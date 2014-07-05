d1db56d9748c7c3634fc7390f7f8b02f27ed5306
Freika <iamfrey@gmail.com>
2014-07-05 21:24:53 +0700

    Added: posts list to category page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,6 +68,7 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
+      @post_category = Category.all.collect{|u| [u.title, u.id]}
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
